db91086c0a1ef745055e922d1d2dc8376e404074
humphreyokoth <humpasp@gmail.com>
2022-09-18 22:41:42 +0300

    Feat:Posting todo item

--- addTodos.php
+++ addTodos.php
@@ -1,15 +1,14 @@
     <?php
 
     //Add to list items to DB
-    $GLOBALS['to_do_item']  = isset($_POST["item"]) ? $_POST["item"] : null;
+    $GLOBALS['to_do_item'] = isset($_POST["item"]) ? $_POST["item"] : null;
     function todo(){
 
-        $to_do_item  = $GLOBALS['to_do_item'];
-        return $to_do_item;
+        $to_do_item = $GLOBALS['to_do_item'];
+        echo $to_do_item;
       
       }
-    // Posting todo item
-    echo "$to_do_item";
+    
     error_log("$to_do_item" . "\n", 3, "./php_error.log");
     ?>
 
